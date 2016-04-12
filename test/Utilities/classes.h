c3a5ab8f71191482fcba7330bbac0922cacb157e
Chris Green <artists@fnal.gov>
2016-04-12 10:06:53 -0500

    Move test directory.
    Account for moved headers and renamed libraries.

--- test/Utilities/classes.h
+++
@@ -1,9 +0,0 @@
-#include "test/Utilities/TypeNameBranchName_t.h"
-#include <vector>
-
-namespace arttest {
-struct empty {};
-struct also_empty {};
-} // namespace arttest
-
-template class std::vector<arttest::MyString>;
