a636d98445191fe374844c5e7782a314cf228fe4
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-01 22:43:24 +0000

    Add minimal Docygen support

--- altCMakeLists.cmake
+++ altCMakeLists.cmake
@@ -52,5 +52,26 @@ add_subdirectory(Modules/AltCMake)
 # tools - e.g. migration scripts
 #add_subdirectory(tools)
 
+#-----------------------------------------------------------------------
+# Documentation
+#
+find_package(Doxygen 1.8)
+if(DOXYGEN_FOUND)
+  set(DOXYGEN_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/Doxygen")
+  configure_file(Doxyfile.in Doxyfile @ONLY)
+  add_custom_command(
+    OUTPUT "${DOXYGEN_OUTPUT_DIR}/html/index.html"
+    COMMAND "${DOXYGEN_EXECUTABLE}" "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+    DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile" canvas_root_io
+    COMMENT "Generating Doxygen docs for ${PROJECT_NAME}"
+    )
+  add_custom_target(doc ALL DEPENDS "${DOXYGEN_OUTPUT_DIR}/html/index.html")
+  install(DIRECTORY "${DOXYGEN_OUTPUT_DIR}/"
+    DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/API"
+    )
+endif()
+
+
 # packaging utility
 #include(UseCPack)
