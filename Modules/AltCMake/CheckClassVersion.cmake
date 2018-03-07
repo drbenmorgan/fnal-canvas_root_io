aef11a878e524e14a20d997bc89ad75804ffe5b7
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-07 10:32:10 +0000

    Improve pyROOT support for CheckClassVersion
    Use query information provided by ROOT's cmake config script to
    detect presence and support for pyROOT. Always FATAL_ERROR if
    either ROOT or pyROOT support not detected.

--- Modules/AltCMake/CheckClassVersion.cmake
+++ Modules/AltCMake/CheckClassVersion.cmake
@@ -8,21 +8,21 @@ set(CCV_DEFAULT_RECURSIVE FALSE
   )
 
 # - Should also check that we can do "import ROOT" in case PYTHONPATH isn't set
-execute_process(COMMAND root-config --has-python
-  RESULT_VARIABLE CCV_ROOT_CONFIG_OK
-  OUTPUT_VARIABLE CCV_ENABLED
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+#execute_process(COMMAND root-config --has-python
+#  RESULT_VARIABLE CCV_ROOT_CONFIG_OK
+#  OUTPUT_VARIABLE CCV_ENABLED
+#  OUTPUT_STRIP_TRAILING_WHITESPACE
+#)
 
-if(NOT CCV_ROOT_CONFIG_OK EQUAL 0)
-  message(FATAL_ERROR "Could not execute root-config successfully to interrogate configuration: exit code ${CCV_ROOT_CONFIG_OK}")
-endif()
+#if(NOT CCV_ROOT_CONFIG_OK EQUAL 0)
+#  message(FATAL_ERROR "Could not execute root-config successfully to interrogate configuration: exit code ${CCV_ROOT_CONFIG_OK}")
+#endif()
 
-if(NOT CCV_ENABLED)
-  message("WARNING: The version of root against which we are building currently has not been built "
-    "with python support: ClassVersion checking is disabled."
-    )
-endif()
+#if(NOT CCV_ENABLED)
+#  message("WARNING: The version of root against which we are building currently has not been built "
+#    "with python support: ClassVersion checking is disabled."
+#    )
+#endif()
 
 function(check_class_version)
   cmake_parse_arguments(CCV
@@ -31,6 +31,21 @@ function(check_class_version)
     "LIBRARIES;REQUIRED_DICTIONARIES"
     ${ARGN}
     )
+
+  # Assume that we will use ROOT's RootConfig
+  if(NOT ROOT_FOUND)
+    message(FATAL_ERROR "check_class_version: ROOT not found\n"
+      "Ensure the project calls:\n"
+      "find_package(ROOT REQUIRED)\n"
+      "before using this function"
+      )
+  elseif(NOT ROOT_python_FOUND)
+    message(FATAL_ERROR "check_class_version: PyROOT not found\n"
+      "ROOT found at `${ROOT_DIR}`\n"
+      "does not provide Python binding to ROOT required by check_class_version"
+      )
+  endif()
+  # ROOT_LIBRARY_DIR *should* then contain the pyroot we need...
 
   if(CCV_LIBRARIES)
     message(FATAL_ERROR "LIBRARIES option not supported at this time: "
@@ -59,11 +74,11 @@ function(check_class_version)
     message(FATAL_ERROR "CHECK_CLASS_VERSION must be called after BUILD_DICTIONARY.")
   endif()
 
-  if(CCV_ENABLED)
+  #if(CCV_ENABLED)
     # Add the check to the end of the dictionary building step.
     add_custom_command(OUTPUT ${dictname}_dict_checked
       # Assumes we're alongside... Might want target...
-      COMMAND ${CHECK_CLASS_VERSION_EXE} ${CCV_EXTRA_ARGS}
+      COMMAND ${CMAKE_COMMAND} -E env "PYTHONPATH=${ROOT_LIBRARY_DIR}:${PYTHONPATH}" ${CHECK_CLASS_VERSION_EXE} ${CCV_EXTRA_ARGS}
       # Might be $<TARGET_FILE:${dictname}_dict>?
       -l $<TARGET_PROPERTY:${dictname}_dict,LIBRARY_OUTPUT_DIRECTORY>/${CMAKE_SHARED_LIBRARY_PREFIX}${dictname}_dict
       -x ${CMAKE_CURRENT_SOURCE_DIR}/classes_def.xml
@@ -83,5 +98,5 @@ function(check_class_version)
     if(CCV_REQUIRED_DICTIONARIES)
       add_dependencies(${dictname}_dict ${CCV_REQUIRED_DICTIONARIES})
     endif()
-  endif()
+    #endif()
 endfunction()
